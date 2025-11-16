be8ae17e8a718267aaebac9a582a7488fb60f597
1cbyc <ei@nsisong.com>
2025-11-16 23:46:01 +0100

    fix(root): Update run.py

--- run.py
+++ run.py
@@ -5,12 +5,12 @@ from app.routes.routes import stocks_blueprint
 
 app.register_blueprint(stocks_blueprint, url_prefix='/stocks')
 
-production = os.environ.get("PRODUCTION", False)
+production = os.environ.get("PRODUCTION", "False").lower() == "true"
 
 
 if __name__ == '__main__':
     if production:
-        app.run(debug=True)
+        app.run(host='0.0.0.0', port=5000, debug=False)
     else:
         app.run(host='127.0.0.1', port=8001, debug=True)
 
